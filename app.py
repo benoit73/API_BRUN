a0fb244245b21f9cdc31e8791e8446c2dde066e5
kiliannnnn <coudurier.kilian@gmail.com>
2025-03-12 17:04:23 +0100

    Test query

--- app.py
+++ app.py
@@ -6,11 +6,26 @@ app = Flask(__name__)
 jsonrpc = JSONRPC(app, "/api")
 
 DB_CONFIG = psycopg2.connect(
-    host="bdd",    
+    host="bdd",
+    # host="10.74.16.190",
     database="mydatabase",  
     user="root",
     password="root"
 )
+
+@jsonrpc.method("App.hello")
+def hello(name: str) -> str:
+    return f"Hello, {name}!"
+
+@jsonrpc.method("App.test_query")
+def test_query() -> list:
+    try:
+        with DB_CONFIG.cursor() as cursor:
+            cursor.execute("SELECT * FROM egapro LIMIT 10;")
+            result = cursor.fetchall()
+        return result
+    except Exception as e:
+        return [str(e)]
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
